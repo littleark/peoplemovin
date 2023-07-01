04259f93aaac01023c5eee56c5652996597e39e9
carlo <littleark@gmail.com>
2023-07-01 11:09:09 +0200

    supporting latest data

--- js/DataMovinInteractions.js
+++ js/DataMovinInteractions.js
@@ -1,8 +1,8 @@
 /*!
  * DataMovinInteractions JavaScript Library v0.4
- * http://datamov.in
+ * http://peoplemov.in
  *
- * (c) Copyright 2011, Carlo Zapponi
+ * (c) Copyright 2011-2023, Carlo Zapponi
  * Temporary licensed under the MIT license.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -11,10 +11,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,294 +25,295 @@
  *
  * Date: Sun Aug 01 00:05:56 2011 (CEST) +0200
  */
-function DataMovinInteractions(){
-	
-	var canvas,
-		orientation,
-		areas,
-		datamovin,
-		mouse={
-			x:0,
-			y:0,
-			scrollTop:0,
-			scrollLeft:0
-		},
-		tm;
+function DataMovinInteractions() {
 
-	var eventsCallbacks={
-		'click':null,
-		'mouseover':null,
-		'mousedown':null,
-		'mouseup':null,
-		'mouseout':null,
-		'processing':function(when,where){}
-	}
-	
-	var scroll={
-		top:0,
-		left:0
-	}
-	
-	this.init=function($datamovin,options) {
-		
-		datamovin=$datamovin;
-		canvas=options.canvas || datamovin.getCanvas();
-		orientation=datamovin.getOrientation();
-		areas=datamovin.getAreas();
-		
-		initEventListeners();
-	}
-	function initEventListeners(){
-		// Init event listeners
-		if (canvas.addEventListener){
-			
-			document.addEventListener('mousemove', documentMouseMove, false);
-			document.addEventListener('mousewheel', documentScrollWheelHandler, false);
-			// Firefox
-			document.addEventListener('DOMMouseScroll', documentScrollWheelHandler, false);
+  var canvas,
+    orientation,
+    areas,
+    datamovin,
+    mouse = {
+      x: 0,
+      y: 0,
+      scrollTop: 0,
+      scrollLeft: 0
+    },
+    tm;
 
-			if(orientation=='vertical') {
-				canvas.addEventListener('click', canvasMouseClickHandlerVertical, false);
-				canvas.addEventListener('mousemove', canvasMouseMoveHandlerVertical, false);
-			} else {
-				canvas.addEventListener('click', canvasMouseClickHandlerHorizontal, false);
-				canvas.addEventListener('getAreas', canvasMouseMoveHandlerHorizontal, false);
-			}
-			canvas.addEventListener('mousedown', canvasMouseDownHandler, false);
-			canvas.addEventListener('mouseup', canvasMouseUpHandler, false);
-			canvas.addEventListener('mouseout', canvasMouseOutHandler, false);
-			
-		} else if (el.attachEvent){
-			
-			document.attachEvent('onmousemove', documentMouseMove, false);
-			document.attachEvent('onmousewheel', documentScrollWheelHandler, false);
-			if(orientation=='vertical') {
-				canvas.attachEvent('onclick', canvasMouseClickHandlerVertical);
-				canvas.attachEvent('onmousemove', canvasMouseMoveHandlerVertical, false);
-			} else {
-				canvas.attachEvent('onclick', canvasMouseClickHandlerHorizontal);
-				canvas.attachEvent('onmousemove', canvasMouseMoveHandlerHorizontal, false);
-			}
-			canvas.attachEvent('onmousedown', canvasMouseDownHandler);
-			canvas.attachEvent('onmouseup', canvasMouseUpHandler);
-			canvas.attachEvent('onmouseout', canvasMouseOutHandler, false);
-		};
-	}
-	this.registerMouseEvents=function(callbacks){
-		for(var i in callbacks){
-			eventsCallbacks[i]=callbacks[i];
-		}
-	}
-	
-	function canvasMouseOutHandler(e){
-		e=initEvent(e);
-		
-		if(eventsCallbacks.mouseout)
-			eventsCallbacks.mouseout.call(e,e);
-			
-		return false;
-	}
-	function documentMouseMove(e) {
-		e=initEvent(e);
-		
-		if(eventsCallbacks.document_mousemove)
-			eventsCallbacks.document_mousemove.call(e,e);
-		
-		return false;	
-	}
-	function documentScrollWheelHandler(e){
-		if (!e) var e = window.event;
-		if(eventsCallbacks.document_scrollwheel)
-			eventsCallbacks.document_scrollwheel.call(e,e);
-	}
-	function canvasMouseMoveHandlerVertical(e){
-		e=initEvent(e);
-		
-		var point,
-			info,
-			left,
-			position;
-		mouse=getPosition(e,canvas);
+  var eventsCallbacks = {
+    'click': null,
+    'mouseover': null,
+    'mousedown': null,
+    'mouseup': null,
+    'mouseout': null,
+    'processing': function(when, where) { }
+  }
 
-		if(mouse.x>=areas.src.x1 && mouse.x<=areas.src.x2) {
-			canvas.style.cursor="pointer";
-			
-			point=datamovin.lookUp(mouse.y,"src");
+  var scroll = {
+    top: 0,
+    left: 0
+  }
 
-			datamovin.findBezier(mouse.x,mouse.y);
+  this.init = function($datamovin, options) {
 
-			info=datamovin.getPointInfo(point,'src');
+    datamovin = $datamovin;
+    canvas = options.canvas || datamovin.getCanvas();
+    orientation = datamovin.getOrientation();
+    areas = datamovin.getAreas();
 
-			if(eventsCallbacks.mouseover) {
-				eventsCallbacks.mouseover.call(e,info);
-			}
-		} else if(mouse.x > areas.src.x2 && mouse.x < areas.dst.x1) {
-			canvas.style.cursor="default";
+    initEventListeners();
+  }
+  function initEventListeners() {
+    // Init event listeners
+    if (canvas.addEventListener) {
 
-			point=datamovin.findBezier(mouse.x,mouse.y);
+      document.addEventListener('mousemove', documentMouseMove, false);
+      document.addEventListener('mousewheel', documentScrollWheelHandler, false);
+      // Firefox
+      document.addEventListener('DOMMouseScroll', documentScrollWheelHandler, false);
 
-			//console.log(point);
+      if (orientation == 'vertical') {
+        canvas.addEventListener('click', canvasMouseClickHandlerVertical, false);
+        canvas.addEventListener('mousemove', canvasMouseMoveHandlerVertical, false);
+      } else {
+        canvas.addEventListener('click', canvasMouseClickHandlerHorizontal, false);
+        canvas.addEventListener('getAreas', canvasMouseMoveHandlerHorizontal, false);
+      }
+      canvas.addEventListener('mousedown', canvasMouseDownHandler, false);
+      canvas.addEventListener('mouseup', canvasMouseUpHandler, false);
+      canvas.addEventListener('mouseout', canvasMouseOutHandler, false);
 
-			if(eventsCallbacks.mouseover) {
-				eventsCallbacks.mouseoverbezier.call(e,point);
-			}
-		} else if(mouse.x>=areas.dst.x1 && mouse.x<=areas.dst.x2) {
-			canvas.style.cursor="pointer";
-			
-			point=datamovin.lookUp(mouse.y,"dst");
+    } else if (el.attachEvent) {
 
-			info=datamovin.getPointInfo(point,'dst');
-			
-			if(eventsCallbacks.mouseover) {
-				eventsCallbacks.mouseover.call(e,info);
-			}
-			
-		} else {
-			canvas.style.cursor="default";
+      document.attachEvent('onmousemove', documentMouseMove, false);
+      document.attachEvent('onmousewheel', documentScrollWheelHandler, false);
+      if (orientation == 'vertical') {
+        canvas.attachEvent('onclick', canvasMouseClickHandlerVertical);
+        canvas.attachEvent('onmousemove', canvasMouseMoveHandlerVertical, false);
+      } else {
+        canvas.attachEvent('onclick', canvasMouseClickHandlerHorizontal);
+        canvas.attachEvent('onmousemove', canvasMouseMoveHandlerHorizontal, false);
+      }
+      canvas.attachEvent('onmousedown', canvasMouseDownHandler);
+      canvas.attachEvent('onmouseup', canvasMouseUpHandler);
+      canvas.attachEvent('onmouseout', canvasMouseOutHandler, false);
+    };
+  }
+  this.registerMouseEvents = function(callbacks) {
+    for (var i in callbacks) {
+      eventsCallbacks[i] = callbacks[i];
+    }
+  }
 
-			if(eventsCallbacks.mouseover) {
-				eventsCallbacks.mouseover.call(e,null);
-			}
-		}
-		
-		if(e.ownerDocument)
-			scroll=getScroll(e);
-		
-		return false;
-	}
+  function canvasMouseOutHandler(e) {
+    e = initEvent(e);
 
-	function canvasMouseDownHandler(e){
-		e=initEvent(e);
-		
-		if(eventsCallbacks.mousedown)
-			eventsCallbacks.mousedown.call(e);
-		return false;
-	}
+    if (eventsCallbacks.mouseout)
+      eventsCallbacks.mouseout.call(e, e);
 
-	function canvasMouseUpHandler(e) {
-		e=initEvent(e);
-		
-		if(eventsCallbacks.mouseup)
-			eventsCallbacks.mouseup.call(e);
-		return false;
-	}
-	function canvasMouseClickHandlerVertical(e) {
-		e=initEvent(e);
-		
-		var point;
-		mouse=getPosition(e,canvas);
-		
-		if(mouse.x>=areas.src.x1 && mouse.x<=areas.src.x2) {
-			point=datamovin.lookUp(mouse.y,"src");
-			if(!datamovin.checkCurrent(point,"src")) {
-				eventsCallbacks.processing.call(e,'start','src');
-				setTimeout(function tm(){
-					datamovin.drawOutFlow(point,e.shiftKey?false:true);
-					if(eventsCallbacks.click)
-						eventsCallbacks.click.call(e,datamovin.getPointInfo(point,'src'));
-					setTimeout(function ttm(){eventsCallbacks.processing.call(e,'end','src');},250);
-				},100);
-			} else {
-				datamovin.clean();
-				if(eventsCallbacks.click)
-					eventsCallbacks.click.call(e,null);
-			}
-		} else if(mouse.x>=areas.dst.x1 && mouse.x<=areas.dst.x2) {
-			point=datamovin.lookUp(mouse.y,"dst");
-			if(!datamovin.checkCurrent(point,"dst")) {
-				eventsCallbacks.processing.call(e,'start','dst');
-				setTimeout(function tm(){
-					datamovin.drawInFlow(point,e.shiftKey?false:true);
-					if(eventsCallbacks.click)
-						eventsCallbacks.click.call(e,datamovin.getPointInfo(point,'dst'));
-					setTimeout(function ttm(){eventsCallbacks.processing.call(e,'end','dst');},250);
-				},100);
-			} else {
-				datamovin.clean();
-				if(eventsCallbacks.click)
-					eventsCallbacks.click.call(e,null);
-			}
-		} else {
-			if(eventsCallbacks.click)
-				eventsCallbacks.click.call(e,-1);
-		}
-		
-		return false;
-		
-	}
+    return false;
+  }
+  function documentMouseMove(e) {
+    e = initEvent(e);
 
-	function canvasMouseClickHandlerHorizontal(e) {
-		e=initEvent(e);
-		
-		
-		var point;
-		mouse=getPosition(e,canvas);
+    if (eventsCallbacks.document_mousemove)
+      eventsCallbacks.document_mousemove.call(e, e);
 
-		if(mouse.y>=areas.src.y1 && mouse.y<=areas.src.y2) {
-			point=datamovin.lookUp(mouse.x,"src");
-			if(!datamovin.checkCurrent(point,"src")) {
-				setTimeout(function tm(){
-					datamovin.drawOutFlow(point,true)
-					click_callback(datamovin.getPointInfo(point,'src'));
-				},5);
-			} else {
-				datamovin.clean();
-			}
-		} else if(mouse.y>=areas.dst.y1 && mouse.y<=areas.dst.y2) {
-			point=datamovin.lookUp(mouse.x,"dst");
-			if(!datamovin.checkCurrent(point,"dst")) {
-				setTimeout(function tm(){
-					datamovin.drawInFlow(point,true)
-					click_callback(datamovin.getPointInfo(point,'dst'));
-				},5);
-			} else {
-				datamovin.clean();
-			}
-		}	
-		
-		return false;
-		
-	}
-	function initEvent(e){
-		if (!e) var e = window.event;
-		if (e.preventDefault)
-		        e.preventDefault();
-		    else
-		        e.returnValue= false;
-		return e;
-	}
-	function findPos(obj) {
-		var curleft = curtop = 0;
-		if (obj.offsetParent) {
-			do {
-				curleft += obj.offsetLeft;
-				curtop += obj.offsetTop;
-			} while (obj = obj.offsetParent);
-		}
-		return [curleft,curtop];
-	}
-	/*
-	*
-	* getPosition needs to be improved to avoid the use of jQuery [only case in which I'm using it...]
-	*
-	*/
-	function getPosition(e) {
+    return false;
+  }
+  function documentScrollWheelHandler(e) {
+    if (!e) var e = window.event;
+    if (eventsCallbacks.document_scrollwheel)
+      eventsCallbacks.document_scrollwheel.call(e, e);
+  }
+  function canvasMouseMoveHandlerVertical(e) {
+    e = initEvent(e);
 
-	    //this section is from http://www.quirksmode.org/js/events_properties.html
-	    var targ;
-	    if (!e)
-	        e = window.event;
-	    if (e.target)
-	        targ = e.target;
-	    else if (e.srcElement)
-	        targ = e.srcElement;
-	    if (targ.nodeType == 3) // defeat Safari bug
-	        targ = targ.parentNode;
+    var point,
+      info,
+      left,
+      position;
+    mouse = getPosition(e, canvas);
 
-	    var offset=findPos(targ);
-	    var x = e.pageX - offset[0];
-	    var y = e.pageY - offset[1];
+    if (mouse.x >= areas.src.x1 && mouse.x <= areas.src.x2) {
+      canvas.style.cursor = "pointer";
 
-	    return {"x": Math.floor(x), "y": Math.floor(y)};
-	};
+      point = datamovin.lookUp(mouse.y, "src");
+
+      datamovin.findBezier(mouse.x, mouse.y);
+
+      info = datamovin.getPointInfo(point, 'src');
+
+      if (eventsCallbacks.mouseover) {
+        eventsCallbacks.mouseover.call(e, info);
+      }
+    } else if (mouse.x > areas.src.x2 && mouse.x < areas.dst.x1) {
+      canvas.style.cursor = "default";
+
+      point = datamovin.findBezier(mouse.x, mouse.y);
+
+      //console.log(point);
+
+      if (eventsCallbacks.mouseover) {
+        eventsCallbacks.mouseoverbezier.call(e, point);
+      }
+    } else if (mouse.x >= areas.dst.x1 && mouse.x <= areas.dst.x2) {
+      canvas.style.cursor = "pointer";
+
+      point = datamovin.lookUp(mouse.y, "dst");
+
+      info = datamovin.getPointInfo(point, 'dst');
+
+      if (eventsCallbacks.mouseover) {
+        eventsCallbacks.mouseover.call(e, info);
+      }
+
+    } else {
+      canvas.style.cursor = "default";
+
+      if (eventsCallbacks.mouseover) {
+        eventsCallbacks.mouseover.call(e, null);
+      }
+    }
+
+    if (e.ownerDocument)
+      scroll = getScroll(e);
+
+    return false;
+  }
+
+  function canvasMouseDownHandler(e) {
+    e = initEvent(e);
+
+    if (eventsCallbacks.mousedown)
+      eventsCallbacks.mousedown.call(e);
+    return false;
+  }
+
+  function canvasMouseUpHandler(e) {
+    e = initEvent(e);
+
+    if (eventsCallbacks.mouseup)
+      eventsCallbacks.mouseup.call(e);
+    return false;
+  }
+  function canvasMouseClickHandlerVertical(e) {
+    e = initEvent(e);
+
+    var point;
+    mouse = getPosition(e, canvas);
+
+    if (mouse.x >= areas.src.x1 && mouse.x <= areas.src.x2) {
+      point = datamovin.lookUp(mouse.y, "src");
+      if (!datamovin.checkCurrent(point, "src")) {
+        eventsCallbacks.processing.call(e, 'start', 'src');
+        setTimeout(function tm() {
+          console.log('point', point)
+          datamovin.drawOutFlow(point, e.shiftKey ? false : true);
+          if (eventsCallbacks.click)
+            eventsCallbacks.click.call(e, datamovin.getPointInfo(point, 'src'));
+          setTimeout(function ttm() { eventsCallbacks.processing.call(e, 'end', 'src'); }, 0);
+        }, 100);
+      } else {
+        datamovin.clean();
+        if (eventsCallbacks.click)
+          eventsCallbacks.click.call(e, null);
+      }
+    } else if (mouse.x >= areas.dst.x1 && mouse.x <= areas.dst.x2) {
+      point = datamovin.lookUp(mouse.y, "dst");
+      if (!datamovin.checkCurrent(point, "dst")) {
+        eventsCallbacks.processing.call(e, 'start', 'dst');
+        setTimeout(function tm() {
+          datamovin.drawInFlow(point, e.shiftKey ? false : true);
+          if (eventsCallbacks.click)
+            eventsCallbacks.click.call(e, datamovin.getPointInfo(point, 'dst'));
+          setTimeout(function ttm() { eventsCallbacks.processing.call(e, 'end', 'dst'); }, 0);
+        }, 100);
+      } else {
+        datamovin.clean();
+        if (eventsCallbacks.click)
+          eventsCallbacks.click.call(e, null);
+      }
+    } else {
+      if (eventsCallbacks.click)
+        eventsCallbacks.click.call(e, -1);
+    }
+
+    return false;
+
+  }
+
+  function canvasMouseClickHandlerHorizontal(e) {
+    e = initEvent(e);
+
+
+    var point;
+    mouse = getPosition(e, canvas);
+
+    if (mouse.y >= areas.src.y1 && mouse.y <= areas.src.y2) {
+      point = datamovin.lookUp(mouse.x, "src");
+      if (!datamovin.checkCurrent(point, "src")) {
+        setTimeout(function tm() {
+          datamovin.drawOutFlow(point, true)
+          click_callback(datamovin.getPointInfo(point, 'src'));
+        }, 5);
+      } else {
+        datamovin.clean();
+      }
+    } else if (mouse.y >= areas.dst.y1 && mouse.y <= areas.dst.y2) {
+      point = datamovin.lookUp(mouse.x, "dst");
+      if (!datamovin.checkCurrent(point, "dst")) {
+        setTimeout(function tm() {
+          datamovin.drawInFlow(point, true)
+          click_callback(datamovin.getPointInfo(point, 'dst'));
+        }, 5);
+      } else {
+        datamovin.clean();
+      }
+    }
+
+    return false;
+
+  }
+  function initEvent(e) {
+    if (!e) var e = window.event;
+    if (e.preventDefault)
+      e.preventDefault();
+    else
+      e.returnValue = false;
+    return e;
+  }
+  function findPos(obj) {
+    var curleft = curtop = 0;
+    if (obj.offsetParent) {
+      do {
+        curleft += obj.offsetLeft;
+        curtop += obj.offsetTop;
+      } while (obj = obj.offsetParent);
+    }
+    return [curleft, curtop];
+  }
+  /*
+  *
+  * getPosition needs to be improved to avoid the use of jQuery [only case in which I'm using it...]
+  *
+  */
+  function getPosition(e) {
+
+    //this section is from http://www.quirksmode.org/js/events_properties.html
+    var targ;
+    if (!e)
+      e = window.event;
+    if (e.target)
+      targ = e.target;
+    else if (e.srcElement)
+      targ = e.srcElement;
+    if (targ.nodeType == 3) // defeat Safari bug
+      targ = targ.parentNode;
+
+    var offset = findPos(targ);
+    var x = e.pageX - offset[0];
+    var y = e.pageY - offset[1];
+
+    return { "x": Math.floor(x), "y": Math.floor(y) };
+  };
 }
--- js/util.js
+++ js/util.js
@@ -1,212 +1,212 @@
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
- 
+
 // requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
- 
+
 // MIT license
- 
+
 (function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+      || window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame)
+    window.requestAnimationFrame = function(callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+        timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+
+  if (!window.cancelAnimationFrame)
+    window.cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
 }());
 
-var Colors=function(){
-		
-	var current=0,
-		rgb=(function(){
-			var rgb=[]
-			for(var i=255;i>=0;i-=5)
-				rgb.push([255,0,i]);
-			for(var i=0;i<=255;i+=5)
-				rgb.push([255,i,0]);
-			for(var i=255;i>=0;i-=5)
-				rgb.push([i,255,0]);
-			for(var i=0;i<=255;i+=5)
-				rgb.push([0,255,i]);
-			for(var i=255;i>=0;i-=5)
-				rgb.push([0,i,255]);
-			return rgb.reverse();
-		}());
-	this.colors_number=rgb.length;
-	this.length=(function(){return rgb.length}());
-	this.getNextColor=function(){
-		current=(current+1)%rgb.length+1;
-		return rgb[current-1][0]+","+rgb[current-1][1]+","+rgb[current-1][2];
-	}
-	this.getColor=function(i){
-		try {
-			return rgb[i][0]+","+rgb[i][1]+","+rgb[i][2];
-		} catch(e) {
-			console.error(i)
-			i=0;
-			return rgb[i][0]+","+rgb[i][1]+","+rgb[i][2];
-		}
-		
-	}
-	this.getHSL=function(i) {
-		return i+","+(100)+"%,50%";
-	}
-}
-function iterateSorted(a,reference) {
-	
-    var keys = [],
-		tmp_values=[],
-		tmp_functions=[];
-		
-    for (var key in a){
-		keys.push(key);
-    }
-	var sorted={};
-	if(reference) {
-		keys.sort(function(a,b){
-			//return reference[a] < reference[b];
-			return reference[a].localeCompare(reference[b]);
-		})
-	} else {
-		keys.sort();	
-	}
-    
+var Colors = function() {
 
-	for(var i = 0; i < keys.length; i++){
-		sorted[keys[i]]=a[keys[i]];
-	}
-	return sorted;
+  var current = 0,
+    rgb = (function() {
+      var rgb = []
+      for (var i = 255; i >= 0; i -= 5)
+        rgb.push([255, 0, i]);
+      for (var i = 0; i <= 255; i += 5)
+        rgb.push([255, i, 0]);
+      for (var i = 255; i >= 0; i -= 5)
+        rgb.push([i, 255, 0]);
+      for (var i = 0; i <= 255; i += 5)
+        rgb.push([0, 255, i]);
+      for (var i = 255; i >= 0; i -= 5)
+        rgb.push([0, i, 255]);
+      return rgb.reverse();
+    }());
+  this.colors_number = rgb.length;
+  this.length = (function() { return rgb.length }());
+  this.getNextColor = function() {
+    current = (current + 1) % rgb.length + 1;
+    return rgb[current - 1][0] + "," + rgb[current - 1][1] + "," + rgb[current - 1][2];
+  }
+  this.getColor = function(i) {
+    try {
+      return rgb[i][0] + "," + rgb[i][1] + "," + rgb[i][2];
+    } catch (e) {
+      console.error(i)
+      i = 0;
+      return rgb[i][0] + "," + rgb[i][1] + "," + rgb[i][2];
+    }
+
+  }
+  this.getHSL = function(i) {
+    return i + "," + (100) + "%,50%";
+  }
+}
+function iterateSorted(a, reference) {
+
+  var keys = [],
+    tmp_values = [],
+    tmp_functions = [];
+
+  for (var key in a) {
+    keys.push(key);
+  }
+  var sorted = {};
+  if (reference) {
+    keys.sort(function(a, b) {
+      //return reference[a] < reference[b];
+      return (reference[a] || a).localeCompare(reference[b] ?? b);
+    })
+  } else {
+    keys.sort();
+  }
+
+
+  for (var i = 0; i < keys.length; i++) {
+    sorted[keys[i]] = a[keys[i]];
+  }
+  return sorted;
 }
 /*Array.prototype.remove=function(i){
-	this.splice(i,1);
+  this.splice(i,1);
 }*/
-function support_canvas(){
-	return !!document.createElement('canvas').getContext
+function support_canvas() {
+  return !!document.createElement('canvas').getContext
 }
-function ie9_bug(){
-	if($.browser.msie)
-		$("<div/>").attr("class","alert").html("You are probably using <span>Internet Explorer 9</span>, great because it supports <span>HTML5</span>! Unfortunately due to some issues you won't be able to fully enjoy <span>people<strong>movin</strong></span>. I hope to fix this soon. Stay tuned for more updates.").prependTo("#contents").fadeIn(1000);
+function ie9_bug() {
+  if ($.browser.msie)
+    $("<div/>").attr("class", "alert").html("You are probably using <span>Internet Explorer 9</span>, great because it supports <span>HTML5</span>! Unfortunately due to some issues you won't be able to fully enjoy <span>people<strong>movin</strong></span>. I hope to fix this soon. Stay tuned for more updates.").prependTo("#contents").fadeIn(1000);
 }
-function first(obj){
-	for(var i in obj)
-		return obj[i];
+function first(obj) {
+  for (var i in obj)
+    return obj[i];
 }
 
-Math.map=function(value, istart, istop, ostart, ostop) {
-	return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+Math.map = function(value, istart, istop, ostart, ostop) {
+  return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
 };
-function Timer(){
-	this.gameTime=0;
-	this.maxStep= .05;
-	this.wallLastTimestamp=0;
+function Timer() {
+  this.gameTime = 0;
+  this.maxStep = .05;
+  this.wallLastTimestamp = 0;
 }
-Timer.prototype.tick=function(){
-	var wallCurrent=Date.now(),
-		wallDelta=(wallCurrent-this.wallLastTimestamp)/1000;
-	this.wallLastTimestamp=wallCurrent;
-	
-	var gameDelta=Math.min(wallDelta,this.maxStep);
-	this.gameTime+=gameDelta;
-	return gameDelta;
-	
+Timer.prototype.tick = function() {
+  var wallCurrent = Date.now(),
+    wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
+  this.wallLastTimestamp = wallCurrent;
+
+  var gameDelta = Math.min(wallDelta, this.maxStep);
+  this.gameTime += gameDelta;
+  return gameDelta;
+
 };
 
-function splitBezier(ctx,x,y,zx,zy) {
+function splitBezier(ctx, x, y, zx, zy) {
 
-	var round=Math.round;
+  var round = Math.round;
 
-	var A={
-		x:x,
-		y:y
-	};
+  var A = {
+    x: x,
+    y: y
+  };
 
-	var B={
-		x:round(zx-(zx-x)/2),
-		y:y
-	}
+  var B = {
+    x: round(zx - (zx - x) / 2),
+    y: y
+  }
 
-	var C={
-		x:round(x+(zx-x)/2),
-		y:zy
-	}
+  var C = {
+    x: round(x + (zx - x) / 2),
+    y: zy
+  }
 
-	var D={
-		x:zx,
-		y:zy
-	}
+  var D = {
+    x: zx,
+    y: zy
+  }
 
-	var t=0.5;
+  var t = 0.5;
 
-	var E={
-		x:round((A.x+B.x)*t),
-		y:round((A.y+B.y)*t)
-	}
+  var E = {
+    x: round((A.x + B.x) * t),
+    y: round((A.y + B.y) * t)
+  }
 
-	var F={
-		x:round((B.x+C.x)*t),
-		y:round((B.y+C.y)*t)
-	}
+  var F = {
+    x: round((B.x + C.x) * t),
+    y: round((B.y + C.y) * t)
+  }
 
-	var G={
-		x:round((C.x+D.x)*t),
-		y:round((C.y+D.y)*t)
-	}
+  var G = {
+    x: round((C.x + D.x) * t),
+    y: round((C.y + D.y) * t)
+  }
 
-	var H={
-		x:round((E.x+F.x)*t),
-		y:round((E.y+F.y)*t)
-	}
+  var H = {
+    x: round((E.x + F.x) * t),
+    y: round((E.y + F.y) * t)
+  }
 
-	var J={
-		x:round((F.x+G.x)*t),
-		y:round((F.y+G.y)*t)
-	}
+  var J = {
+    x: round((F.x + G.x) * t),
+    y: round((F.y + G.y) * t)
+  }
 
-	var K={
-		x:round((H.x+J.x)*t),
-		y:round((H.y+J.y)*t)
-	}
+  var K = {
+    x: round((H.x + J.x) * t),
+    y: round((H.y + J.y) * t)
+  }
 
-	/*
-		E = (A+B)/2
-		F = (B+C)/2
-		G = (C+D)/2
-		H = (E+F)/2
-		J = (F+G)/2
-		K = (H+J)/2
+  /*
+    E = (A+B)/2
+    F = (B+C)/2
+    G = (C+D)/2
+    H = (E+F)/2
+    J = (F+G)/2
+    K = (H+J)/2
 
-		A,E,H,K and K,J,G,D.
+    A,E,H,K and K,J,G,D.
 
-	*/
+  */
 
-	ctx.moveTo(A.x,A.y)
-	
-	ctx.bezierCurveTo(
-		E.x,E.y,
-		H.x,H.y,
-		K.x,K.y
-	);
+  ctx.moveTo(A.x, A.y)
 
-	ctx.bezierCurveTo(
-		J.x,J.y,
-		G.x,G.y,
-		D.x,D.y
-	);
+  ctx.bezierCurveTo(
+    E.x, E.y,
+    H.x, H.y,
+    K.x, K.y
+  );
 
-	
+  ctx.bezierCurveTo(
+    J.x, J.y,
+    G.x, G.y,
+    D.x, D.y
+  );
+
+
 }
